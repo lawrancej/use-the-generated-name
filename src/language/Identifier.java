79b30dd869d9ce76eba8ad70e5f29e36e0c98a59
Joey Lawrance <lawrancej@wit.edu>
2015-05-17 23:03:21 -0400

    Process rules as a map from identifier to a node.
    Changing the rule implies replacing the rule for an identifier.

--- src/language/Identifier.java
+++ src/language/Identifier.java
@@ -1,15 +1,16 @@
 package language;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Identifier implements Node {
 	private static final Map<String, Identifier> instances = new HashMap<String, Identifier>();
 	public final String label;
-	protected Node rule;
 	private Identifier(String label) {
 		this.label = label;
-		rule = EmptySet.getInstance();
+		// Default rule
+		Rule.create(this, EmptySet.getInstance());
 	}
 	public static Identifier getInstance(String label) {
 		if (! instances.containsKey(label)) {
@@ -17,21 +18,25 @@ public class Identifier implements Node {
 		}
 		return instances.get(label);
 	}
+	public static Collection<Identifier> list() {
+		return instances.values();
+	}
 	@Override
 	public <T> T accept(Visitor<T> v) {
 		return v.visit(this);
 	}
 	public void derive(Node... nodes) {
+		Node rhs = Rule.getInstance(this).child.right;
 		if (nodes.length == 0) {
-			rule = Or.getInstance(rule, Rule.getInstance(this, EmptyString.getInstance()));
+			Rule.create(this, Or.getInstance(rhs, EmptyString.getInstance()));
 		} else if (nodes.length == 1) {
-			rule = Or.getInstance(rule, Rule.getInstance(this, nodes[0]));
+			Rule.create(this, Or.getInstance(rhs, nodes[0]));
 		} else {
 			Node result = Sequence.getInstance(nodes[0], nodes[1]);
 			for (int i = 2; i < nodes.length; i++) {
 				result = Sequence.getInstance(result, nodes[i]);
 			}
-			rule = Or.getInstance(rule, Rule.getInstance(this, result));
+			Rule.create(this, Or.getInstance(rhs, result));
 		}
 	}
 }
--- src/language/Rule.java
+++ src/language/Rule.java
@@ -1,29 +1,25 @@
 package language;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+// Equals operator in a grammar
 public class Rule extends Operator<Pair<Identifier,Node>> {
-	private static Map<Pair<Identifier, Node>, Rule> instances = new HashMap<Pair<Identifier,Node>, Rule>();
-	private static Map<Identifier, ArrayList<Rule>> instanceMap = new HashMap<Identifier, ArrayList<Rule>>();
+	private static Map<Identifier, Rule> instances = new HashMap<Identifier, Rule>();
 	private Rule(Pair<Identifier, Node> children) {
 		this.child = children;
 	}
-	public static Node getInstance(Identifier nonterminal, Node rule) {
-		Pair<Identifier, Node> children = new Pair<Identifier, Node>(nonterminal, rule);
-		if (! instances.containsKey(children)) {
-			Rule r = new Rule(children);
-			instances.put(children, r);
-			if (! instanceMap.containsKey(nonterminal)) {
-				instanceMap.put(nonterminal, new ArrayList<Rule>());
-			}
-			instanceMap.get(nonterminal).add(r);
-		}
-		return instances.get(children);
+	
+	// Create rule for the identifier
+	public static Node create(Identifier id, Node node) {
+		Pair<Identifier, Node> children = new Pair<Identifier, Node>(id, node);
+		instances.put(id, new Rule(children));
+		return instances.get(id);
 	}
-	public static ArrayList<Rule> getRulesForIdentifier(Identifier id) {
-		return instanceMap.get(id);
+	
+	// Get rule for the identifier
+	public static Rule getInstance(Identifier id) {
+		return instances.get(id);
 	}
 
 	@Override
