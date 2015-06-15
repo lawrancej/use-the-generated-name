3e0ee6bb234ee91ad946e91f10c78e632ffd3c95
Joey Lawrance <lawrancej@wit.edu>
2015-06-14 21:39:35 -0400

    Treap using hashcode priority is kinda bad.

---
+++ src/test/TreapTest.java
@@ -0,0 +1,88 @@
+package test;
+
+import org.junit.Test;
+
+import util.Treap;
+
+public class TreapTest {
+	
+	@Test
+	public void testInt() {
+		Treap<String> treap = Treap.insert("" + 0, null);
+		for (int i = 1; i < 2525; i++) {
+			treap = Treap.insert("" + i, treap);
+		}
+		System.out.println(treap);
+		System.out.println(Treap.height(treap));
+		System.out.println(Treap.size(0, treap));
+	}
+
+	@Test
+	public void test() {
+		Treap<String> hey = Treap.create("hello");
+		System.out.println(hey);
+		hey = Treap.insert("welcome", hey);
+		System.out.println(hey);
+		hey = Treap.insert("howdy", hey);
+		System.out.println(hey);
+		hey = Treap.insert("greetings", hey);
+		System.out.println(hey);
+		hey = Treap.insert("aloha", hey);
+		System.out.println(hey);
+		hey = Treap.insert("mahalo", hey);
+		System.out.println(hey);
+		hey = Treap.insert("wow", hey);
+		System.out.println(hey);
+		hey = Treap.insert("good to see you", hey);
+		System.out.println(hey);
+		hey = Treap.insert("hola", hey);
+		System.out.println(hey);
+		hey = Treap.insert("hi", hey);
+		System.out.println(hey);
+		hey = Treap.insert("yo", hey);
+		System.out.println(hey);
+		hey = Treap.insert("sup", hey);
+		System.out.println(hey);
+		hey = Treap.insert("derp", hey);
+		System.out.println(hey);
+		hey = Treap.insert("wat", hey);
+		System.out.println(hey);
+		hey = Treap.insert("omg", hey);
+		System.out.println(hey);
+		hey = Treap.insert("hey", hey);
+		System.out.println(hey);
+		hey = Treap.insert("doom", hey);
+		System.out.println(hey);
+		hey = Treap.insert("boss", hey);
+		System.out.println(hey);
+		hey = Treap.insert("drat", hey);
+		System.out.println(hey);
+		hey = Treap.insert("why", hey);
+		System.out.println(hey);
+		hey = Treap.insert("because", hey);
+		System.out.println(hey);
+		hey = Treap.insert("do", hey);
+		System.out.println(hey);
+		hey = Treap.insert("or do not", hey);
+		System.out.println(hey);
+		hey = Treap.insert("there is", hey);
+		System.out.println(hey);
+		hey = Treap.insert("no", hey);
+		System.out.println(hey);
+		hey = Treap.insert("try", hey);
+		System.out.println(hey);
+		hey = Treap.insert("catch", hey);
+		System.out.println(hey);
+		hey = Treap.insert("block", hey);
+		System.out.println(hey);
+		hey = Treap.insert("for", hey);
+		System.out.println(hey);
+		hey = Treap.insert("you", hey);
+		System.out.println(hey);
+		hey = Treap.insert("too", hey);
+		System.out.println(hey);
+		System.out.println(Treap.height(hey));
+		System.out.println(Treap.size(0, hey));
+	}
+
+}
---
+++ src/util/Treap.java
@@ -0,0 +1,101 @@
+package util;
+
+// A treap implementation
+public class Treap<T extends Comparable<T>> {
+	public T key;
+	public Treap<T> left;
+	public Treap<T> right;
+	
+	public static <T extends Comparable<T>> int priority(Treap<T> treap) {
+		if (treap == null) {
+			return Integer.MIN_VALUE;
+		}
+		return treap.key.hashCode();
+	}
+	
+	private Treap(T key, Treap<T> left, Treap<T> right) {
+		this.key = key;
+		this.left = left;
+		this.right = right;
+	}
+	
+	public static <T extends Comparable<T>> Treap<T> create(T key) {
+		return new Treap<T>(key, null, null);
+	}
+	
+	public static <T extends Comparable<T>> Treap<T> insert(T item, Treap<T> treap) {
+		Treap<T> result = treap;
+		if (treap == null) {
+			result = create(item);
+			return result;
+		}
+		int comparison = item.compareTo(treap.key);
+		if (comparison < 0) {
+			treap.left = insert(item, treap.left);
+			if (priority(treap.left) > priority(treap)) {
+				result = rotateRight(treap);
+			}
+		} else if (comparison > 0) {
+			treap.right = insert(item, treap.right);
+			if (priority(treap.right) > priority(treap)) {
+				result = rotateLeft(treap);
+			}
+		}
+		return result;
+	}
+	
+	public static <T extends Comparable<T>> Treap<T> rotateLeft(Treap<T> treap) {
+		Treap<T> pivot = treap.right;
+		treap.right = pivot.left;
+		pivot.left = treap;
+		treap = pivot;
+		return treap;
+	}
+	public static <T extends Comparable<T>> Treap<T> rotateRight(Treap<T> treap) {
+		Treap<T> pivot = treap.left;
+		treap.left = pivot.right;
+		pivot.right = treap;
+		treap = pivot;
+		return treap;
+	}
+	
+	public static <T extends Comparable<T>> StringBuffer stringify(Treap<T> treap, StringBuffer buffer) {
+		if (treap != null) {
+			buffer.append("(");
+			buffer.append(treap.key);
+			if (treap.left == null && treap.right == null) {
+				buffer.append(")");
+				return buffer;
+			}
+			buffer.append(" ");
+			stringify(treap.left,buffer);
+			buffer.append(" ");
+			stringify(treap.right,buffer);
+			buffer.append(")");
+		}
+		else {
+			buffer.append("null");
+		}
+		return buffer;
+	}
+	
+	public static <T extends Comparable<T>> int height(Treap<T> treap) {
+		if (treap == null) return 0;
+		return Math.max(height(treap.left) + 1, height(treap.right) + 1);
+	}
+	
+	public static <T extends Comparable<T>> int size(int start, Treap<T> treap) {
+		int result = start;
+		if (treap == null) return result;
+		result ++;
+		result = size(result, treap.left);
+		result = size(result, treap.right);
+		return result;
+	}
+	
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		stringify(this, buffer);
+		return buffer.toString();
+	}
+}
