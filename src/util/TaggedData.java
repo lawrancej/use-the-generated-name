377132a9eac99019dc170b82299df926cd4ac95a
Joey Lawrance <lawrancej@wit.edu>
2015-07-07 21:51:00 -0400

    Track allocations in TaggedData

--- src/util/TaggedData.java
+++ src/util/TaggedData.java
@@ -4,9 +4,11 @@ package util;
 public class TaggedData<T> {
 	public final int tag;
 	public final T data;
+	public static int allocations = 0;
 	protected TaggedData(int type, T data) {
 		this.tag = type;
 		this.data = data;
+		allocations++;
 	}
 	// Handy shortcut for the constructor call
 	public static <T> TaggedData<T> create(int type, T data) {
