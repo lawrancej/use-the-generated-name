f48205fb61626f40fc0b8c637880f4be63978002
Joey Lawrance <lawrancej@wit.edu>
2015-07-11 22:45:29 -0400

    GraphViz only emits unique nodes and edges.

--- src/languageV2/traversal/GraphViz.java
+++ src/languageV2/traversal/GraphViz.java
@@ -12,64 +12,82 @@ public class GraphViz extends AbstractVisitor<StringBuffer> {
 	public GraphViz(Language g) {
 		super(g);
 	}
-	private Set<String> arrows;
-	private void drawArrow(Object from, int to) {
+	private Set<Node<?,?>> nodes = new HashSet<Node<?,?>>();
+	private Set<String> edges = new HashSet<String>();
+	private void drawEdge(Object from, int to) {
 		String arrow = String.format("%s -> %s;\n", from, to);
-		if (!arrows.contains(arrow)) {
-			arrows.add(arrow);
+		if (!edges.contains(arrow)) {
+			edges.add(arrow);
 			buffer.append(arrow);
 		}
 	}
 	public StringBuffer symbol(Node<Character,Character> language) {
-		if (language == Language.any) {
-			buffer.append(String.format("%s [label=\"Any\"];\n", language.hashCode()));
-		} else {
-			buffer.append(String.format("%s [label=\"'%c'\"];\n", language.hashCode(), language.left));
+		if (!nodes.contains(language)) {
+			nodes.add(language);
+			if (language == Language.any) {
+				buffer.append(String.format("%s [label=\"Any\"];\n", language.hashCode()));
+			} else if (language.left == language.right) {
+				buffer.append(String.format("%s [label=\"'%c'\"];\n", language.hashCode(), language.left));
+			} else {
+				buffer.append(String.format("%s [label=\"'%c'..'%c'\"];\n", language.hashCode(), language.left, language.right));
+			}
 		}
 		return buffer;
 	}
 	public StringBuffer list(Node<Node<?,?>,Node<?,?>> list) {
-		if (list == Language.empty) {
-			buffer.append(String.format("%s [label=\"&epsilon;\"];\n", list.hashCode()));
-			return buffer;
+		if (!nodes.contains(list)) {
+			nodes.add(list);
+			if (list == Language.empty) {
+				buffer.append(String.format("%s [label=\"&epsilon;\"];\n", list.hashCode()));
+				return buffer;
+			}
+			buffer.append(String.format("%s [label=\"{List|{<left> L|<right> R}}\"];\n", list.hashCode()));
+			g.accept(this, list.left);
+			drawEdge(list.hashCode() + ":left", list.left.hashCode());
+			g.accept(this, list.right);
+			drawEdge(list.hashCode() + ":right", list.right.hashCode());
 		}
-		buffer.append(String.format("%s [label=\"{List|{<left> L|<right> R}}\"];\n", list.hashCode()));
-		g.accept(this, list.left);
-		drawArrow(list.hashCode() + ":left", list.left.hashCode());
-		g.accept(this, list.right);
-		drawArrow(list.hashCode() + ":right", list.right.hashCode());
 		return buffer;
 	}
 	public StringBuffer loop(Node<Node<?,?>,Node<?,?>> language) {
-		buffer.append(String.format("%s [label=\"Loop\"];\n", language.hashCode()));
-		g.accept(this, language.left);
-		drawArrow(language.hashCode(), language.left.hashCode());
+		if (!nodes.contains(language)) {
+			nodes.add(language);
+			buffer.append(String.format("%s [label=\"Loop\"];\n", language.hashCode()));
+			g.accept(this, language.left);
+			drawEdge(language.hashCode(), language.left.hashCode());
+		}
 		return buffer;
 	}
 	public StringBuffer set(Node<Node<?,?>,Node<?,?>> set) {
-		if (set == Language.reject) {
-			buffer.append(String.format("%s [label=\"Reject\"];\n", set.hashCode()));
-			return buffer;
+		if (!nodes.contains(set)) {
+			nodes.add(set);
+			if (set == Language.reject) {
+				buffer.append(String.format("%s [label=\"Reject\"];\n", set.hashCode()));
+				return buffer;
+			}
+			buffer.append(String.format("%s [label=\"Set\"];\n", set.hashCode()));
+			g.accept(this, set.left);
+			drawEdge(set.hashCode(), set.left.hashCode());
+			g.accept(this, set.right);
+			drawEdge(set.hashCode(), set.right.hashCode());
 		}
-		buffer.append(String.format("%s [label=\"Set\"];\n", set.hashCode()));
-		g.accept(this, set.left);
-		drawArrow(set.hashCode(), set.left.hashCode());
-		g.accept(this, set.right);
-		drawArrow(set.hashCode(), set.right.hashCode());
 		return buffer;
 	}
 	public StringBuffer id(Id id) {
-		if (id.left == null) {
-			buffer.append(String.format("%s [label=\"Id\"];\n", id.hashCode()));
-		} else {
-			buffer.append(String.format("%s [label=\"Id '%s'\"];\n", id.hashCode(), id.left));
+		if (!nodes.contains(id)) {
+			nodes.add(id);
+			if (id.left == null) {
+				buffer.append(String.format("%s [label=\"Id\"];\n", id.hashCode()));
+			} else {
+				buffer.append(String.format("%s [label=\"Id '%s'\"];\n", id.hashCode(), id.left));
+			}
 		}
 		return buffer;
 	}
 	public StringBuffer rule(Id id, Node<?,?> rhs) {
 		this.id(id);
 		g.accept(this, rhs);
-		drawArrow(id.hashCode(), rhs.hashCode());
+		drawEdge(id.hashCode(), rhs.hashCode());
 		return buffer;
 	}
 	public StringBuffer bottom() {
@@ -82,12 +100,12 @@ public class GraphViz extends AbstractVisitor<StringBuffer> {
 		return buffer;
 	}
 	public void begin() {
-		arrows = new HashSet<String>();
+		edges.clear();
+		nodes.clear();
 		buffer = new StringBuffer();
 		buffer.append("digraph f { \n");
 		buffer.append("node [shape=record];\n");
 	}
-	@Override
 	public StringBuffer end(StringBuffer accumulator) {
 		buffer.append(" }");
 		return accumulator;
